3d93897e75aa684b70f995b037a140c12155b080
Justin Overfelt <joverfelt@gmail.com>
2014-02-19 22:08:43 -0500

    add basic query functionality - error handling still needs work

--- src/passport/core.clj
+++ src/passport/core.clj
@@ -1,6 +1,51 @@
-(ns passport.core)
+(ns passport.core
+  (:require [cheshire.core :as json]
+            [clj-http.lite.client :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:const ^:private api-username "OPEN_CNAM_SID")
+(def ^:const ^:private api-token "OPEN_CNAM_TOKEN")
+(def ^:const ^:private base-url "https://api.opencnam.com/v2/phone/")
+(def ^:const ^:private error-text-400 "400: Invalid phone number")
+(def ^:const ^:private error-text-401 "401: Incorrect/invalid Professional Tier credentials")
+(def ^:const ^:private error-text-402 "402: Insufficient funds for Professional Tier query")
+(def ^:const ^:private error-text-403 "403: Hourly limit for hobbyist tier exceeded")
+(def ^:const ^:private error-text-404 "404: No CNAM information available for phone number")
+(def ^:const ^:private error-text-generic "Something went wrong. Please check response status")
+
+(defn- get-env-username []
+  (get (System/getenv) api-username ""))
+
+(defn- get-env-token []
+  (get (System/getenv) api-token ""))
+
+(defn- handle-response [resp]
+  (let [status (:status resp)]
+    (cond
+      (= status 200) (json/parse-string (:body resp) true)
+      (= status 400) (json/generate-string {:error error-text-400 :status status})
+      (= status 401) (json/generate-string {:error error-text-401 :status status})
+      (= status 402) (json/generate-string {:error error-text-402 :status status})
+      (= status 403) (json/generate-string {:error error-text-403 :status status})
+      (= status 404) (json/generate-string {:error error-text-404 :status status})
+      :else (json/generate-string {:error error-text-generic :status status}))))
+
+(defn- passport-get [url username token]
+  (-> url
+      (http/get {:accept :json :query-params {"format" "json"} :basic-auth [username token] :throw-exceptions false})
+      (handle-response)))
+
+(defn caller-id
+  "Takes the phone number to query and
+   and optional token and sid. Token and 
+   sid can be specified in the OPEN_CNAM_TOKEN
+   and OPEN_CNAM_SID env variables or passed in
+   to this function if use of the professional tier is desired"
+
+  ([phone]
+    (let [url (str base-url phone)
+          username (get-env-username)
+          token (get-env-token)]
+      (passport-get url username token)))
+
+  ([phone username token]
+    (passport-get (str base-url phone) username token)))
