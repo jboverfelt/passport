976bac808571c56c7ab6cf56eb643f5909a42c7b
Justin Overfelt <joverfelt@gmail.com>
2014-02-20 17:54:54 -0500

    changing to use regular clj-http and cleaning up a bit

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.3.1"]
-                 [clj-http-lite "0.2.0"]]
+                 [clj-http "0.7.9"]]
   :dev-dependencies [[clj-http-fake "0.7.8"]])
--- src/passport/core.clj
+++ src/passport/core.clj
@@ -1,15 +1,15 @@
 (ns passport.core
   (:require [cheshire.core :as json]
-            [clj-http.lite.client :as http]))
+            [clj-http.client :as http]))
 
 (def ^:const ^:private api-username "OPEN_CNAM_SID")
 (def ^:const ^:private api-token "OPEN_CNAM_TOKEN")
 (def ^:const ^:private base-url "https://api.opencnam.com/v2/phone/")
-(def ^:const ^:private error-text-400 "400: Invalid phone number")
-(def ^:const ^:private error-text-401 "401: Incorrect/invalid Professional Tier credentials")
-(def ^:const ^:private error-text-402 "402: Insufficient funds for Professional Tier query")
-(def ^:const ^:private error-text-403 "403: Hourly limit for hobbyist tier exceeded")
-(def ^:const ^:private error-text-404 "404: No CNAM information available for phone number")
+(def ^:const ^:private error-text-400 "Invalid phone number")
+(def ^:const ^:private error-text-401 "Incorrect/invalid Professional Tier credentials")
+(def ^:const ^:private error-text-402 "Insufficient funds for Professional Tier query")
+(def ^:const ^:private error-text-403 "Hourly limit for hobbyist tier exceeded")
+(def ^:const ^:private error-text-404 "No CNAM information available for phone number")
 (def ^:const ^:private error-text-generic "Something went wrong. Please check response status")
 
 (defn- get-env-username []
@@ -22,12 +22,12 @@
   (let [status (:status resp)]
     (cond
       (= status 200) (json/parse-string (:body resp) true)
-      (= status 400) (json/generate-string {:error error-text-400 :status status})
-      (= status 401) (json/generate-string {:error error-text-401 :status status})
-      (= status 402) (json/generate-string {:error error-text-402 :status status})
-      (= status 403) (json/generate-string {:error error-text-403 :status status})
-      (= status 404) (json/generate-string {:error error-text-404 :status status})
-      :else (json/generate-string {:error error-text-generic :status status}))))
+      (= status 400) {:error error-text-400 :status status}
+      (= status 401) {:error error-text-401 :status status}
+      (= status 402) {:error error-text-402 :status status}
+      (= status 403) {:error error-text-403 :status status}
+      (= status 404) {:error error-text-404 :status status}
+      :else          {:error error-text-generic :status status})))
 
 (defn- passport-get [url username token]
   (-> url
