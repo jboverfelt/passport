de944888bd8ab05e6b268d6e5185d63e8ebe62b3
Justin Overfelt <joverfelt@gmail.com>
2014-02-20 21:40:48 -0500

    adding more tests

--- test/passport/core_test.clj
+++ test/passport/core_test.clj
@@ -1,23 +1,14 @@
 (ns passport.core-test
   (:require [clojure.test :refer :all]
             [passport.core :refer :all]
-            [clj-http.fake :refer [with-fake-routes]]))
+            [clj-http.fake :refer [with-fake-routes-in-isolation]]))
 
-(def ^:const ^:private base-url "https://api.opencnam.com/v2/phone/")
-(def ^:const ^:private error-text-400 "Invalid phone number")
-(def ^:const ^:private error-text-401 "Incorrect/invalid Professional Tier credentials")
-(def ^:const ^:private error-text-402 "Insufficient funds for Professional Tier query")
-(def ^:const ^:private error-text-403 "Hourly limit for hobbyist tier exceeded")
-(def ^:const ^:private error-text-404 "No CNAM information available for phone number")
-(def ^:const ^:private error-text-generic "Something went wrong. Please check response status")
-(def found-url (str base-url "+16502530000"))
-(def not-found-url (str base-url "+16502530001"))
-(def bogus-phone-url (str base-url "bogus"))
+(def route-regex #"https://api\.opencnam\.com/v2/phone/.*")
 
 (deftest phone-found
   (testing "request for a phone in the CNAM db"
-    (with-fake-routes
-      {found-url
+    (with-fake-routes-in-isolation
+      {route-regex
       (fn [_] {:status 200 :headers {} :body "{\n  \"price\": 0, \n  \"uri\": \"/v2/phone/%2B16502530000\", 
         \n  \"created\": \"2012-10-12T06:53:05.1    94858\", 
         \n  \"number\": \"+16502530000\", \n  \"updated\": \"2014-02-20T18:59:27.279412\", \n  \"name\": \"GOOGLE INC\"\n}"})}
@@ -27,27 +18,45 @@
 
 (deftest phone-invalid
   (testing "request for a phone number that is in an invalid form"
-    (with-fake-routes
-      {bogus-phone-url
-      (fn [_] {:status 400 :error error-text-400})}
+    (with-fake-routes-in-isolation
+      {route-regex
+      (fn [_] {:status 400 :headers {} :body ""})}
 
       (is (:error (caller-id "bogus")))
       (is (= (:status (caller-id "bogus")) 400)))))
 
 (deftest phone-not-found
   (testing "request for a phone number not in the CNAM db"
-    (with-fake-routes
-      {not-found-url
-      (fn [_] {:status 401 :error error-text-401})}
+    (with-fake-routes-in-isolation
+      {route-regex
+      (fn [_] {:status 404 :headers {} :body ""})}
 
       (is (:error (caller-id "+16502530001")))
       (is (= (:status (caller-id "+16502530001")) 404)))))
 
 (deftest invalid-credentials
   (testing "specifying invalid Professional Tier credentials"
-    (with-fake-routes
-      {found-url
-      (fn [_] {:status 404 :error error-text-404})}
+    (with-fake-routes-in-isolation
+      {route-regex
+      (fn [_] {:status 401 :headers {} :body ""})}
 
       (is (:error (caller-id "+16502530001" "test" "test")))
       (is (= (:status (caller-id "+16502530001" "test" "test")) 401)))))
+
+(deftest insufficient-funds  
+  (testing "insufficient funds in account for Professional Tier query"
+    (with-fake-routes-in-isolation
+      {route-regex
+      (fn [_] {:status 402 :headers {} :body ""})}
+
+      (is (:error (caller-id "+16502530000")))
+      (is (= (:status (caller-id "+16502530000")) 402)))))
+
+(deftest hourly-limit-exceeded 
+  (testing "insufficient funds in account for Professional Tier query"
+    (with-fake-routes-in-isolation
+      {route-regex
+      (fn [_] {:status 403 :headers {} :body ""})}
+
+      (is (:error (caller-id "+16502530000")))
+      (is (= (:status (caller-id "+16502530000")) 403)))))
