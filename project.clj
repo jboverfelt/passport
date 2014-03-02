5f9574982a997a6e4fd4fb11b98f5317b4945212
Justin Overfelt <joverfelt@gmail.com>
2014-03-02 14:15:38 -0500

    updating clj-http to latest version

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.3.1"]
-                 [clj-http "0.7.9"]]
+                 [clj-http "0.9.0"]]
   :profiles {:dev {:dependencies [[clj-http-fake "0.7.8"]]}})
