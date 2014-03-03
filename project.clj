549995d082ce917979d69f9b6c581c754e211d34
Justin Overfelt <joverfelt@gmail.com>
2014-03-02 21:05:23 -0500

    adding cloverage plugin to project

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.3.1"]
                  [clj-http "0.9.0"]]
+  :plugins [[lein-cloverage "1.0.2"]]
   :profiles {:dev {:dependencies [[clj-http-fake "0.7.8"]]}})
