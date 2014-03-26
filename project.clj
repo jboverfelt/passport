3b487d43bb44eb7ac997e69795f91b63db6358e3
Justin Overfelt <joverfelt@gmail.com>
2014-03-25 21:14:44 -0400

    updating clojure to 1.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jboverfelt/passport"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [cheshire "5.3.1"]
                  [clj-http "0.9.0"]]
   :plugins [[lein-cloverage "1.0.2"]]
