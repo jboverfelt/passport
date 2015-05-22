87257ad30cc434d05e4a6403909d6c564a055cd8
Justin Overfelt <joverfelt@gmail.com>
2015-05-22 16:38:32 -0400

    update project version and update deps

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject passport "0.1.1"
+(defproject passport "0.1.2-SNAPSHOT"
   :description "Clojure client for the OpenCNAM API"
   :url "https://github.com/jboverfelt/passport"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "5.3.1"]
-                 [clj-http "1.0.1"]]
-  :plugins [[lein-cloverage "1.0.2"]]
+                 [cheshire "5.4.0"]
+                 [clj-http "1.1.2"]]
+  :plugins [[lein-cloverage "1.0.6"]]
   :profiles {:dev {:dependencies [[clj-http-fake "1.0.1"]]}})
